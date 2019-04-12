464831579959d393ac213616a85fe29e6fd57258
Kevin Qi <kqi@amino.com>
2019-04-11 22:28:50 -0700

    get keyboard input and scoring working

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { MdClear } from "react-icons/md";
 import { IoIosSquareOutline, IoIosRadioButtonOff } from "react-icons/io";
 import { GiTriangleTarget } from "react-icons/gi";
+import useKey from "use-key-hook";
 import _ from "lodash";
 import "./App.css";
 
-function RandomIcon() {
-  const [iconName, setIconName] = React.useState<string>("circle");
-
-  React.useEffect(() => {
-    let timer: any;
-    timer = setInterval(() => {
-      const index = _.random(0, 3);
-      const mapping: { [key: number]: string } = {
-        0: "x",
-        1: "circle",
-        2: "square",
-        3: "triangle"
-      };
-      const iconName: string = mapping[index];
-      setIconName(iconName);
-    }, 1000);
-
-    return () => {
-      window.clearInterval(timer);
-    };
-  }, []);
-
+function RandomIcon({ iconName }: any) {
+  if (iconName === "empty") {
+    return null;
+  }
   if (iconName === "x") {
     return <MdClear />;
   }
@@ -42,16 +25,62 @@ function RandomIcon() {
   throw new Error("Invalid index");
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="d-flex align-items-center justify-content-center width-full height-full">
-        <div style={{ fontSize: 100 }}>
-          <RandomIcon />
-        </div>
+function App() {
+  const [countCorrect, setCountCorrect] = React.useState(0);
+  const [countAttempts, setCountAttempts] = React.useState(0);
+
+  const [iconName, setIconName] = React.useState<string>("empty");
+  React.useEffect(
+    () => {
+      let timer: any;
+      timer = setInterval(() => {
+        const index = _.random(0, 3);
+        const mapping: { [key: number]: string } = {
+          0: "x",
+          1: "circle",
+          2: "square",
+          3: "triangle"
+        };
+        const iconName: string = mapping[index];
+        setIconName(iconName);
+        setCountAttempts(countAttempts + 1);
+      }, 1000);
+
+      return () => {
+        window.clearInterval(timer);
+      };
+    },
+    [countAttempts, iconName]
+  );
+
+  useKey(
+    (pressedKey: number, event: any) => {
+      const mapping: { [key: string]: string } = {
+        ArrowLeft: "square",
+        ArrowRight: "circle",
+        ArrowUp: "triangle",
+        ArrowDown: "x"
+      };
+      const iconPressed = mapping[event.key];
+      console.log(iconPressed, iconName);
+      if (iconPressed === iconName) {
+        setCountCorrect(countCorrect + 1);
+      }
+    },
+    {},
+    { dependencies: [iconName, countCorrect] }
+  );
+
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center width-full height-full">
+      <div style={{ fontSize: 40 }}>
+        {countCorrect} / {countAttempts}
       </div>
-    );
-  }
+      <div style={{ fontSize: 100 }}>
+        <RandomIcon iconName={iconName} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
